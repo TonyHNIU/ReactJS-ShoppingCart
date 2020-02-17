9003872923ac4e10bc4933c0d689db9b036db3b8
HUI NIU <hui-niu@outlook.com>
2020-02-17 23:46:57 +1300

    Modification App.tsx. Created dependencies provider under di folder

--- src/presentation/app/App.tsx
+++ src/presentation/app/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import ProductList from "../products/components/ProductList";
+import * as DependenciesProvider from "../../di/DependenciesProvider";
 
 const App: React.FC = () => {
-  return <div></div>;
+  const productsPresenter = DependenciesProvider.provideProductsPresenter();
+
+  return (
+    <div>
+      <ProductList
+        productsPresenter={productsPresenter}
+        onAddProductToCart={}
+      />
+    </div>
+  );
 };
 
 export default App;
