dd8c97b48ba8a8ce4e12a7b4b487d3c94bf4a0b1
HUI NIU <hui-niu@outlook.com>
2020-02-18 00:24:06 +1300

    Modification index.tsx file.

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./presentation/app/App";
 import theme from "./presentation/theme";
+import * as DependenciesProvider from "./di/DependenciesProvider";
+
+const cartPresenter = DependenciesProvider.provideCartPresenter();
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    <App cartPresenter={cartPresenter} />
   </ThemeProvider>,
   document.querySelector("#root")
 );
