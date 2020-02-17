b9a1fdcd5c37674c89ac900dbe93ad9f264da6b3
HUI NIU <hui-niu@outlook.com>
2020-02-17 22:58:27 +1300

    Extend existing data. Saving imgages to imgbb host.

---
+++ src/data/products/ProductInMemoryRepository.tsx
@@ -0,0 +1,65 @@
+import ProductRepository from "../../domain/products/ProductRepository";
+import Product from "../../domain/products/Product";
+
+const products = [
+  {
+    id: "1",
+    image: "https://i.ibb.co/9WbMSzX/Plumpy-Nut.jpg",
+    title: "[10%off] Plumpy Nut",
+    price: 25.76
+  },
+  {
+    id: "2",
+    image: "https://i.ibb.co/D1Q1j0r/Measles-Vaccine.jpg",
+    title: "[10%off] Measles Vaccine",
+    price: 90.51
+  },
+  {
+    id: "3",
+    image: "https://i.ibb.co/VBzDtt9/Therapeutic-Milk.jpg",
+    title: "[10%off] Therapeutic Milk",
+    price: 56.14
+  },
+  {
+    id: "4",
+    image: "https://i.ibb.co/Qbpv9j4/Pencils.jpg",
+    title: "[10%off] Pencils",
+    price: 13.9
+  },
+  {
+    id: "5",
+    image: "https://i.ibb.co/Jntvy6S/School-Bags.jpg",
+    title: "[10%off] School Bags",
+    price: 19.45
+  },
+  {
+    id: "6",
+    image: "https://i.ibb.co/ThPT6p9/Apple-Watch.jpg",
+    title: "[10%off] Apple Watch",
+    price: 929
+  },
+  {
+    id: "7",
+    image: "https://i.ibb.co/1b1ZWYB/Beats-Pro.jpg",
+    title: "[10%off] Beats Pro",
+    price: 399.95
+  },
+  {
+    id: "8",
+    image: "https://i.ibb.co/10k1Pt3/Alienware.jpg",
+    title: "[10%off] Alienware Laptop",
+    price: 6498.98
+  },
+  {
+    id: "9",
+    image: "https://i.ibb.co/LvNC5DZ/Powerbank.jpg",
+    title: "[10%off] Belkin Power Bank",
+    price: 49.99
+  },
+  {
+    id: "10",
+    image: "https://i.ibb.co/BG4KYfq/Giftcard.jpg",
+    title: "[10%off] Apple Gift Card",
+    price: 18
+  }
+];
